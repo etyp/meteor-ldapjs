208aa148d9d7c1d5c5aa8542eb0a8a535b60c29e
Eric Typaldos <etypaldos@pappasgroup.com>
2015-02-22 13:26:54 -0500

    Renamed global var for package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'typ:ldapjs',
-  version: '0.7.1',
+  version: '0.7.3',
   summary: 'Meteor package wrapper for the ldapjs Npm module https://www.npmjs.com/package/ldapjs',
   git: 'https://github.com/typ90/meteor-ldapjs',
   documentation: 'README.md'
@@ -17,6 +17,6 @@ Package.onUse(function (api) {
   ], ['server']);
   
   api.export([
-    'ldapjs'
+    'MeteorWrapperLdapjs'
   ]);
 });
